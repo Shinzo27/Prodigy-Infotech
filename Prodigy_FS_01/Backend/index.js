a93c22bb629ac78f6cb0e146a30e27b4e96ab3e5
Shinzo27 <20bmiit031@gmail.com>
2024-07-15 00:18:56 +0530

    Init

--- Prodigy_FS_01/Backend/index.js
+++ Prodigy_FS_01/Backend/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import userRouter from './Routes/User.js'
 
 const app = express()
 const PORT = process.env.PORT || 8000
@@ -6,6 +7,8 @@ const PORT = process.env.PORT || 8000
 app.get('/', (req,res)=>{
     res.send("Hello world")
 })
+
+app.use('/api/v1/user', userRouter)
 
 app.listen(PORT, ()=>{
     console.log("Server started at port "+ PORT);
